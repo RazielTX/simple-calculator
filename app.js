56a57b9bfd563e3ccec621e7d40703d973955a80
razieltx <deyberbejarano97@gmail.com>
2022-08-25 12:06:09 -0500

    Commas added to the show function

--- app.js
+++ app.js
@@ -2,10 +2,16 @@ const entry = [];
 const entryScreen = document.getElementById("entryScreen");
 const resultScreen = document.getElementById("resultScreen");
 
+function separator(num) {
+    let str = num.split(".");
+    str[0] = str[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    return str.join(".");
+}
+
 function showEntry() {
 
-
     const entryShown = entry.map((element) => {
+        
         switch(element) {
             case "/":
                 return' <i class="fa-solid fa-divide fa-xs"></i> ';
@@ -19,12 +25,22 @@ function showEntry() {
                 return element;
         }
     })
+    const regexNumbers = /\d+(\.\d+)*|(<.*>|[\(\)])/g;
+    const numbersTogether = entryShown.join('').match(regexNumbers);
+
+    const finalShown = numbersTogether.map((element) => {
+        if((/\d+(\.\d+)*/).test(element)) {
+            return separator(element);
+        } else {
+            return element;
+        }
+    })
 
 
     // The first section of the function (above) is just for style purposes.
 
 
-    entryScreen.innerHTML = `${entryShown.join('')}`
+    entryScreen.innerHTML = `${finalShown.join(' ')}`;
 }
 
 function pressKey(key) {
@@ -41,10 +57,23 @@ function reset() {
 function calc(str = entry.join('')) {
     // const entryCalc = new Function(str);
     const result = eval(str);
-    resultScreen.innerHTML = `${result}`;
+    const resultCommas = separator(result.toString());
+    resultScreen.innerHTML = `${resultCommas}`;
 }
 
 function deleteBack() {
     entry.pop();
     showEntry();
 }
+
+
+// const numb=213231221;
+
+// function separator(numb) {
+//     var str = numb.toString().split(".");
+//     console.log(str);
+//     str[0] = str[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+//     return str.join(".");
+// }
+
+// console.log(separator(numb))
